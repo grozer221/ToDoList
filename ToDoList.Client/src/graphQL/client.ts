d293bbc9df2734f16ba7a97380e6f3776fdf74cf
Vitaliy Vaskivskiy <ipz204_vvyu@student.ztu.edu.ua>
2022-06-01 09:36:01 +0300

    updated grahph api url#2

--- ToDoList.Client/src/graphQL/client.ts
+++ ToDoList.Client/src/graphQL/client.ts
@@ -2,7 +2,7 @@ import {ApolloClient, InMemoryCache} from '@apollo/client';
 import {schema} from './schema';
 
 export const client = new ApolloClient({
-    uri: !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? '/graphql' : process.env.REACT_APP_GRAPH_QL_API_URL,
+    uri: !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? process.env.REACT_APP_GRAPH_QL_API_URL : '/graphql',
     cache: new InMemoryCache(),
     credentials: 'include',
     defaultOptions: {
